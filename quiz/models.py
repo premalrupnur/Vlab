778e22a330d5c522fe1d1f0f2b120204dc78f20e
premalrupnur <premalrupnur2001@gmail.com>
2020-12-06 15:05:25 +0530

    Adds more templates

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.enums import TextChoices
+from django.db.models.fields import TextField
 
 # Create your models here.
+class procedures(models.Model):
+    header = models.TextField()
+    list_items = models.TextField()
--- quiz/views.py
+++ quiz/views.py
@@ -2,7 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,"typing.html",{})
+    return render(request,"typing.html")
 
 def index(request):
-    return render(request,"index.html",{})
+    return render(request,"index.html")
+
+def objective(request):
+    return render(request,"Objective.html")
+
+def resources(request):
+    return render(request,"Resources.html")
+
+def experiment(request):
+    return render(request,"Experiments.html")
+
+def aim(request):
+    return render(request,"Experiments/aim.html")
+
+def procedure(request):
+    return render(request,"Experiments/procedure.html")
+
+def quiz(request):
+    return render(request,"Experiments/quiz.html")
+
+def references(request):
+    return render(request,"Experiments/references.html")
+
+def simulation(request):
+    return render(request,"Experiments/simulation.html")
+
+def theory(request):
+    return render(request,"Experiments/theory.html")
